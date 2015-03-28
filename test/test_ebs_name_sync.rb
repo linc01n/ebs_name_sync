a180e6f629743747f9f4ba669f7a8df0fb71e46c
Lincoln Lee <git@lincoln.hk>
2015-03-28 19:13:20 +0800

    Remove dummy test

--- test/test_ebs_name_sync.rb
+++ test/test_ebs_name_sync.rb
@@ -4,8 +4,4 @@ class TestEbsNameSync < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::EbsNameSync::VERSION
   end
-
-  def test_it_does_something_useful
-    assert false
-  end
 end
